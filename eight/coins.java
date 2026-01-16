ee544fc5a58a2652354db154c3f29778e0ce0fdf
adityaD <dalal.aditya.2456@gmail.com>
2026-01-16 23:49:46 +0530

    Add files via upload

---
+++ eight/coins.java
@@ -0,0 +1,17 @@
+package eight;
+
+import java.util.Scanner;
+
+public class coins {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            long n=sc.nextLong();
+            long k=sc.nextLong();
+            if(n%2==0) System.out.println("YES");
+            else{
+            if(k%2==1) System.out.println("YES");
+            else System.out.println("NO");
+        }}}}
